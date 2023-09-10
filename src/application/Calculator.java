fe2a9da9f4c2d14995f95fd127886baaee60b4d4
viniciussnitram <vinicius.martins.vsm@outlook.com>
2023-09-10 17:54:02 -0300

    refactor: Added attributes and methods to Calculator Class. Attributes: both private double, numberOne and numberTwo. Methods: gets and sets of attributes, sum, subtract, divide and multiply

--- src/application/Calculator.java
+++ src/application/Calculator.java
@@ -1,4 +1,38 @@
 package application;
 
 public class Calculator {
+    private double numberOne;
+    private double numberTwo;
+
+    public void setNumberOne(double numberOne) {
+        this.numberOne = numberOne;
+    }
+
+    public double getNumberOne() {
+        return numberOne;
+    }
+
+    public void setNumberTwo(double numberTwo) {
+        this.numberTwo = numberTwo;
+    }
+
+    public double getNumberTwo() {
+        return numberTwo;
+    }
+
+    public double sum(double numberOne, double numberTwo) {
+        return numberOne + numberTwo;
+    }
+
+    public double subtract(double numberOne, double numberTwo) {
+        return numberOne - numberTwo;
+    }
+
+    public double divide(double numberOne, double numberTwo) {
+        return numberOne / numberTwo;
+    }
+
+    public double multiply(double numberOne, double numberTwo) {
+        return numberOne * numberTwo;
+    }
 }
