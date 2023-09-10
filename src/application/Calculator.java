60f0cfe6ef88372fc3386e86b8ed32d5675526ac
viniciussnitram <vinicius.martins.vsm@outlook.com>
2023-09-10 19:46:16 -0300

    refactor: The calculator is done. Implemented the function to calculate button, clear button and close button. The calculator have methods to sum, subtract, divide and multiply. Maybe after I will change the style and implement the inputs to only accept numbers

--- src/application/Calculator.java
+++ src/application/Calculator.java
@@ -11,16 +11,47 @@ public class Calculator extends JDialog {
     private JLabel labelNumberTwo;
     private JTextField inputNumberOne;
     private JTextField inputNumberTwo;
-    private JComboBox comboBox1;
+    private JComboBox comboOperation;
     private JButton calculateButton;
     private JButton clearButton;
     private JButton closeButton;
+    private JPanel Calculator;
+    private JLabel labelOperation;
 
     public Calculator() {
+        setContentPane(Calculator);
+        setModal(true);
+        getRootPane().setDefaultButton(closeButton);
+
         closeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                onClose();
+            }
+        });
 
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                inputNumberOne.setText("");
+                inputNumberTwo.setText("");
+                comboOperation.setSelectedIndex(0);
+            }
+        });
+
+        calculateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setNumberOne(Double.parseDouble(inputNumberOne.getText()));
+                setNumberTwo(Double.parseDouble(inputNumberTwo.getText()));
+
+                switch (comboOperation.getSelectedIndex()) {
+                    case 1 -> JOptionPane.showMessageDialog(null, sum(getNumberOne(), getNumberTwo()));
+                    case 2 -> JOptionPane.showMessageDialog(null, subtract(getNumberOne(), getNumberTwo()));
+                    case 3 -> JOptionPane.showMessageDialog(null, divide(getNumberOne(), getNumberTwo()));
+                    case 4 -> JOptionPane.showMessageDialog(null, multiply(getNumberOne(), getNumberTwo()));
+                    default -> JOptionPane.showMessageDialog(null, "There is no operation type selected.");
+                }
             }
         });
     }
@@ -55,6 +86,10 @@ public class Calculator extends JDialog {
 
     public double multiply(double numberOne, double numberTwo) {
         return numberOne * numberTwo;
+    }
+
+    public void onClose() {
+        dispose();
     }
 
     public static void main(String[] args) {
